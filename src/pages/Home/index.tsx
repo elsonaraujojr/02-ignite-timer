17cc9b40cebbdd9229991597ca24797ef8ea1a8b
elsonaraujojr <elsonaraujojr@gmail.com>
2025-03-22 22:04:14 -0400

    Mudando countdown

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -54,11 +54,16 @@ export function Home() {
   const activeCycle = cycles.find((cycle) => cycle.id === activeCycleId);
 
   useEffect(() => {
+    let interval: number;
     if (activeCycle) {
-      setInterval(() => {
+      interval = setInterval(() => {
         setAmountSecondsPassed(
           differenceInSeconds(new Date(), activeCycle.startDate))
       }, 1000)
+    }
+
+    return () => {
+      clearInterval(interval)
     }
   }, [activeCycle])
 
@@ -73,6 +78,7 @@ export function Home() {
 
     setCycles((state) => [...state, newCycle])
     setActiveCycleId(newCycle.id)
+    setAmountSecondsPassed(0)
 
     reset();
   }
@@ -95,6 +101,15 @@ export function Home() {
 
   const task = watch('task')
   const isSubmitDisabled = !task
+
+  useEffect(() => {
+    if (activeCycle) {
+      document.title = `${minutes}:${seconds}`
+    }
+
+
+  }, [minutes, seconds, activeCycle])
+
 
   return (
     <HomeContainer>
