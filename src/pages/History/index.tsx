61a0ecd614e451bb78785234eb4c1f0236a27a51
ejunior <ejunior@tcerr.tc.br>
2025-04-07 17:43:14 -0400

    Listando o histórico

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -9,9 +9,6 @@ export function History() {
     <HistoryContainer>
       <h1>Meu histórico</h1>
 
-      <pre>
-        {JSON.stringify(cycles, null, 2)}
-      </pre>
 
       <HistoryList>
         <table>
@@ -24,62 +21,21 @@ export function History() {
             </tr>
           </thead>
           <tbody>
-            <tr>
-              <td>Tarefa 000</td>
-              <td>2 minutos</td>
-              <td>Há 2 meses</td>
-              <td>
-                <Status statusColor="green">Concluído</Status>
-              </td>
-            </tr>
-            <tr>
-              <td>Tarefa 001</td>
-              <td>2 minutos</td>
-              <td>Há 2 meses</td>
-              <td>
-                <Status statusColor="green">Concluído</Status>
-              </td>
-            </tr>
-            <tr>
-              <td>Tarefa 002</td>
-              <td>2 minutos</td>
-              <td>Há 2 meses</td>
-              <td>
-                <Status statusColor="green">Concluído</Status>
-              </td>
-            </tr>
-            <tr>
-              <td>Tarefa 003</td>
-              <td>2 minutos</td>
-              <td>Há 2 meses</td>
-              <td>
-                <Status statusColor="green">Concluído</Status>
-              </td>
-            </tr>
-            <tr>
-              <td>Tarefa 004</td>
-              <td>2 minutos</td>
-              <td>Há 2 meses</td>
-              <td>
-                <Status statusColor="green">Concluído</Status>
-              </td>
-            </tr>
-            <tr>
-              <td>Tarefa 005</td>
-              <td>2 minutos</td>
-              <td>Há 2 meses</td>
-              <td>
-                <Status statusColor="yellow">Em adamento</Status>
-              </td>
-            </tr>
-            <tr>
-              <td>Tarefa 006</td>
-              <td>2 minutos</td>
-              <td>Há 2 meses</td>
-              <td>
-                <Status statusColor="red">Interrompodo</Status>
-              </td>
-            </tr>
+            {cycles.map(cycle => {
+              return (
+                <tr key={cycle.id}>
+                  <td>{cycle.task}</td>
+                  <td>{cycle.minutesAmount} minutos</td>
+                  <td>{cycle.startDate.toISOString()}</td>
+                  <td>
+                    <Status statusColor={cycle.finishedDate ? 'green' : cycle.interruptDate ? 'red' : 'yellow'}>
+                      {cycle.finishedDate ? 'Concluído' : cycle.interruptDate ? 'Interrompido' : 'Em andamento'}
+                    </Status>
+                  </td>
+                </tr>
+              )
+            })}
+
           </tbody>
         </table>
 
